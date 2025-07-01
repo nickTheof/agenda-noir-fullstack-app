0f16185a504e765b86b9ce2fab8b8c1866897a34
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-01 22:35:00 +0300

    feat(repository): initial setup for User, VerificationToken, PasswordResetToken repository interfaces

---
+++ src/main/java/gr/aueb/cf/projectmanagementapp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.projectmanagementapp.repository;
+
+import gr.aueb.cf.projectmanagementapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    Optional<User> findByUuid(String uuid);
+    Optional<User> findByUsername(String username);
+}
