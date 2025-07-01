17dcd53681fdb6a555c85c71d9b020f562d46d9f
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-01 21:33:47 +0300

    feat: add OpenAPI configuration with Swagger documentation
    - Added OpenAPIDefinition with basic API info and security requirement
    - Configured bearerAuth security scheme for JWT authentication
    - Defined initial tags for Authentication and Users endpoints

---
+++ src/main/java/gr/aueb/cf/projectmanagementapp/core/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.projectmanagementapp.core;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+        info = @Info(title = "My Project Management REST API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth"),
+        tags = { //TODO: Add the remaining OPEN API tags for swagger
+                @Tag(name = "Authentication", description = "User authentication endpoints"),
+                @Tag(name = "Users", description = "User management endpoints"),
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
