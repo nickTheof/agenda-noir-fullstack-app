4a54bf332491b6ba4d928c68549e896ef469841c
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-02 22:20:35 +0300

    fix(swagger): remove global @SecurityRequirement to allow unprotected endpoints
    Some endpoints are public and should not require authentication.
    @SecurityRequirement will now be applied at the controller level instead.

--- src/main/java/gr/aueb/cf/projectmanagementapp/core/OpenApiConfig.java
+++ src/main/java/gr/aueb/cf/projectmanagementapp/core/OpenApiConfig.java
@@ -3,13 +3,11 @@ package gr.aueb.cf.projectmanagementapp.core;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @OpenAPIDefinition(
         info = @Info(title = "My Project Management REST API", version = "1.0"),
-        security = @SecurityRequirement(name = "bearerAuth"),
         tags = { //TODO: Add the remaining OPEN API tags for swagger
                 @Tag(name = "Authentication", description = "User authentication endpoints"),
                 @Tag(name = "Users", description = "User management endpoints"),
