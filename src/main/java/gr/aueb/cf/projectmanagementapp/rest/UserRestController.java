e4865e360ee4ce577c163819743a2226ea8c60cf
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-03 23:14:36 +0300

    feat(user): add role management endpoints
    - Add GET /users/{uuid}/roles to retrieve a user's roles
    - Add PATCH /users/{uuid}/roles to update user roles

--- src/main/java/gr/aueb/cf/projectmanagementapp/rest/UserRestController.java
+++ src/main/java/gr/aueb/cf/projectmanagementapp/rest/UserRestController.java
@@ -162,7 +162,7 @@ public class UserRestController {
     public ResponseEntity<UserReadOnlyDTO> insertUser(
             @Valid @RequestBody UserRegisterDTO dto,
             BindingResult bindingResult
-    ) throws ValidationException, AppObjectAlreadyExistsException, AppServerException, AppObjectNotFoundException {
+    ) throws ValidationException, AppObjectAlreadyExistsException {
         if (bindingResult.hasErrors()) {
             LOGGER.warn("Validation errors for UserRegisterDTO: {}", bindingResult.getAllErrors());
             throw new ValidationException(bindingResult);
@@ -634,5 +634,130 @@ public class UserRestController {
         LOGGER.warn("User not found: {}", uuid);
         throw e;
          }
+    }
+
+    @GetMapping("/{uuid}/roles")
+    @PreAuthorize("@authorizationService.hasAuthority(authentication.principal, 'READ_USER') || @authorizationService.hasOwnership(authentication.principal, #uuid)")
+    @Operation(
+            summary = "Get roles for a specific user",
+            description = "Retrieves all roles assigned to the user identified by the given UUID.",
+            parameters = {
+                    @Parameter(
+                            name = "uuid",
+                            description = "The unique identifier of the user to retrieve all its roles",
+                            required = true,
+                            example = "baba3f82-7b0f-4440-9893-a2f76169802c",
+                            in = ParameterIn.PATH,
+                            schema = @Schema(type = "string", format = "uuid")
+                    )
+            }
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of users roles returned successfully",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = RoleReadOnlyDTO.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Token not found or expired. Authentication failed.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden access. Authenticated user has not permission to access the specific resource.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User not found.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorDTO.class))
+                    ),
+            }
+    )
+    public ResponseEntity<List<RoleReadOnlyDTO>> getUserRoles(
+        @PathVariable String uuid
+    ) throws AppObjectNotFoundException {
+        try {
+            List<RoleReadOnlyDTO> userRoles = userService.findAllUserRoles(uuid);
+            return new ResponseEntity<>(userRoles, HttpStatus.OK);
+        } catch (AppObjectNotFoundException e) {
+            LOGGER.warn("User not found trying fetched user roles: {}", uuid);
+            throw e;
+        }
+    }
+
+    @PatchMapping("/{uuid}/roles")
+    @PreAuthorize("@authorizationService.hasAuthority(authentication.principal, 'UPDATE_ROLE')")
+    @Operation(
+            summary = "Update roles of a specific user",
+            description = "Update the roles assigned to the user identified by the given UUID.",
+            parameters = {
+                    @Parameter(
+                            name = "uuid",
+                            description = "The unique identifier of the user to update all its roles",
+                            required = true,
+                            example = "baba3f82-7b0f-4440-9893-a2f76169802c",
+                            in = ParameterIn.PATH,
+                            schema = @Schema(type = "string", format = "uuid")
+                    )
+            }
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "List of updated users roles returned successfully",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = RoleReadOnlyDTO.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Token not found or expired. Authentication failed.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden access. Authenticated user has not permission to access the specific resource.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorDTO.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User or Role not found.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ApiErrorDTO.class))
+                    ),
+            }
+    )
+    public ResponseEntity<List<RoleReadOnlyDTO>> updateUserRoles(
+            @PathVariable String uuid,
+            @Valid @RequestBody UserRoleInsertDTO roleDTO,
+            BindingResult bindingResult
+    ) throws AppObjectNotFoundException {
+        try {
+            if (bindingResult.hasErrors()) {
+                LOGGER.warn("Validation errors in updating user roles: {}", bindingResult.getAllErrors());
+            }
+            List<RoleReadOnlyDTO> userRoles = userService.changeUserRoles(uuid, roleDTO);
+            return new ResponseEntity<>(userRoles, HttpStatus.OK);
+        } catch (AppObjectNotFoundException e) {
+            LOGGER.warn("User not found trying update user roles: {}", uuid);
+            throw e;
+        }
     }
 }
