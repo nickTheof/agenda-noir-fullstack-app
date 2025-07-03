aef7c1b2f3ff19175d4f8e091b35110ea097944d
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-03 21:29:57 +0300

    feat(dto): add bean validation and swagger schema annotations at RoleCreate and RoleUpdate DTOs.

--- src/main/java/gr/aueb/cf/projectmanagementapp/dto/RoleCreateDTO.java
+++ src/main/java/gr/aueb/cf/projectmanagementapp/dto/RoleCreateDTO.java
@@ -1,9 +1,18 @@
 package gr.aueb.cf.projectmanagementapp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public record RoleCreateDTO(
+        @Schema(description = "The role's name", example = "USER_MANAGER")
+        @NotBlank(message = "The name of the role is a required field")
         String name,
+
+        @Schema(description = "The list including all the permissions of the role", example = "['READ_USER', 'CREATE_ROLE']")
+        @NotEmpty(message = "At least one permission is required")
         List<String> permissions
 ) {
 }
