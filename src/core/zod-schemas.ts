ebf88b7a7521f2057311dc5445057ac153ec4b05
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-08 22:18:12 +0300

    feat(schemas): add zod schemas for auth forms - Login Form - Register User Form - Password Recovery Request Form - Password Reset Form After Recovery request

---
+++ src/core/zod-schemas.ts
@@ -0,0 +1,37 @@
+import {z} from "zod/v4";
+
+export const passwordSchema = z.string()
+    .min(8, 'Password must be at least 8 characters')
+    .regex(/[A-Z]/, 'Must contain at least one uppercase letter')
+    .regex(/[a-z]/, 'Must contain at least one lowercase letter')
+    .regex(/[0-9]/, 'Must contain at least one number')
+    .regex(/[@#$%!^&*]/, 'Must contain at least one special character')
+
+export const passwordRecoveryRequestSchema = z.object({
+    username: z.email().min(1, "Username has to be a valid email address"),
+})
+
+export const passwordResetSchema = z.object({
+    newPassword: passwordSchema,
+    confirmPassword: z.string(),
+}).refine(data => data.newPassword === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+})
+
+export const loginSchema = z.object({
+    username: z.email().min(1, "Username is required"),
+    password: z.string().min(1, "Password is required"),
+})
+
+export const registerSchema = z.object({
+    username: z.email("Username must be a valid email address").min(1, "Username is required"),
+    password: passwordSchema,
+    confirmPassword: z.string(),
+    firstname: z.string().min(1, "First name is required"),
+    lastname: z.string().min(1, "First name is required")}
+).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+});
+
