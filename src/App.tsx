a964247152e878a740ddc6ae55dc97053c578557
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-17 13:40:37 +0300

    feat(routes): add routes for protected pages and a global not-found page - Project Pages - Ticket Pages - Settings Page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,14 @@ import VerifyAccountPage from "@/pages/auth/VerifyAccountPage.tsx";
 import RegisterUserPage from "@/pages/auth/RegisterUserPage.tsx";
 import PasswordRecoveryPage from "@/pages/auth/PasswordRecoveryPage.tsx";
 import PasswordResetPage from "@/pages/auth/PasswordResetPage.tsx";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx";
+import ProjectsPage from "@/pages/dashboard/ProjectsPage.tsx";
+import DashboardLayout from "@/components/DashboardLayout.tsx";
+import ProjectCreatePage from "@/pages/dashboard/ProjectCreatePage.tsx";
+import SettingsPage from "@/pages/dashboard/SettingsPage.tsx";
+import TicketsPage from "@/pages/dashboard/TicketsPage.tsx";
+import TicketModePage from "@/pages/dashboard/TicketModePage.tsx";
+import NotFoundPage from "@/pages/dashboard/NotFoundPage.tsx";
 
 function App() {
 
@@ -27,6 +35,28 @@ function App() {
                                 <Route path="password-recovery" element={<PasswordRecoveryPage />} />
                                 <Route path="reset-password" element={<PasswordResetPage />} />
                             </Route>
+                        </Route>
+                        <Route element={<DashboardLayout/>}>
+                            <Route path="dashboard" element={<ProtectedRoute />}>
+                                <Route path="projects">
+                                    <Route path="view" element={<ProjectsPage />}></Route>
+                                    <Route path="new" element={<ProjectCreatePage />}></Route>
+                                    <Route path=":projectUuid">
+                                        <Route path="tickets">
+                                            <Route index element={<TicketsPage/>} />
+                                            <Route path="new" element={<TicketModePage mode="create" />} />
+                                            <Route path=":ticketUuid/update" element={<TicketModePage mode="edit" />}></Route>
+                                        </Route>
+                                    </Route>
+                                </Route>
+
+                                <Route path="profile">
+                                    <Route path="settings" element={<SettingsPage />}></Route>
+                                </Route>
+                            </Route>
+                        </Route>
+                        <Route element={<Layout />}>
+                            <Route path="*" element={<NotFoundPage />} />
                         </Route>
                     </Routes>
                 </BrowserRouter>
