fc8b22bde2a341e4a148a9abc8cbbabbf7379444
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-19 09:24:14 +0300

    feat(api): add function for executing api call for getting paginated users

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,4 +1,4 @@
-import type {ChangePasswordFields, RegisterUserResponse} from "@/core/types.ts";
+import type {ChangePasswordFields, PaginatedResponse, RegisterUserResponse, UserResponse} from "@/core/types.ts";
 
 const BASE_URL: string = import.meta.env.VITE_API_URL;
 
@@ -101,4 +101,34 @@ export async function deactivateUserAccount(
         }
         throw new Error(detail);
     }
+}
+
+
+export async function getUsersPaginated(
+    token: string,
+    page: number,
+    limit: number,
+): Promise<PaginatedResponse<UserResponse>> {
+    const res = await fetch(`${BASE_URL}/users/filtered`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            page: page - 1,
+            size: limit,
+        })
+    });
+    if (!res.ok) {
+        let detail = "Fetch users details failed";
+        try {
+            const data = await res.json();
+            if (typeof data?.message =="string") detail = data?.message;
+        } catch (err) {
+            console.error(err);
+        }
+        throw new Error(detail);
+    }
+    return await res.json();
 }
