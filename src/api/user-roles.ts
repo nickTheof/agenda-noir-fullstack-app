0de3287036b7536445bceaff183963f012379587
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-07-20 22:03:42 +0300

    feat(api): add function for editing the custom roles of a user

--- src/api/user-roles.ts
+++ src/api/user-roles.ts
@@ -1,4 +1,4 @@
-import type {UserRoleResponse} from "@/core/types.ts";
+import type {UserRoleEditFormValues, UserRoleResponse} from "@/core/types.ts";
 
 const BASE_URL: string = import.meta.env.VITE_API_URL;
 
@@ -11,6 +11,28 @@ export async function fetchUserRoles(token: string, userUuid: string): Promise<U
     });
     if (!res.ok) {
         let detail = "Fetch user roles failed";
+        try {
+            const data = await res.json();
+            if (typeof data?.message =="string") detail = data?.message;
+        } catch (err) {
+            console.error(err);
+        }
+        throw new Error(detail);
+    }
+    return await res.json();
+}
+
+export async function patchUserRoles(token: string, userUuid: string, data: UserRoleEditFormValues): Promise<UserRoleResponse> {
+    const res = await fetch(`${BASE_URL}/users/${userUuid}/roles`, {
+        method: 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    if (!res.ok) {
+        let detail = "Update user roles failed";
         try {
             const data = await res.json();
             if (typeof data?.message =="string") detail = data?.message;
